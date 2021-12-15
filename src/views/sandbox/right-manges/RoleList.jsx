8ffa37630670c0d550c66bc7fb0a391f1a2ddc6e
“kunwang6” <cdcut7173@gmail.com>
2021-12-15 09:29:46 +0800

    feat:完成了角色列表的渲染

--- src/views/sandbox/right-manges/RoleList.jsx
+++ src/views/sandbox/right-manges/RoleList.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { Table , Button } from 'antd'
+import axios from 'axios'
 
 export default function RoleList() {
+    const [dataSource, setdataSource] = useState([])
+    useEffect(() => {
+      axios.get('http://localhost:5000/roles').then(res=>{
+          setdataSource(res.data)
+      })
+    }, [])
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+           render:id=>{
+               return <b>{id}</b>
+           }
+          },
+          {
+            title: '角色名称',
+            dataIndex: 'roleName',
+          },
+          {
+            title: '操作',
+           render:(item)=>{
+               return <div>
+                    <Button type="primary" >编辑</Button>
+                    <Button type="primary" danger >删除</Button>
+               </div>
+           }
+          },
+        ]
     return (
         <div>
-            RoleList
+            <Table dataSource={dataSource} columns={columns} rowKey={(item)=>item.id}></Table>
         </div>
     )
 }
